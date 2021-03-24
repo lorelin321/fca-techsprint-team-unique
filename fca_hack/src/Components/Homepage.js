10e7b64d7b243b6ce582b577dcb1bf6e56d60d94
Becky O'Keefe <rebecca.okeefe@fujitsu.com>
2021-03-24 10:50:52 +0000

    Adding access to the your infomation page

--- fca_hack/src/Components/Homepage.js
+++ fca_hack/src/Components/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import './Homepage.css';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     return(
@@ -11,7 +12,7 @@ const Homepage = () => {
                 <p>Welcome to our app, here you can find help, start to plan your journey and also start to build an identity</p>
                 <button><h3>Find out more</h3></button><br/>
                 <button><h3>Find help locally</h3></button>
-                <button><h3>Build an identity</h3></button>
+                <Link to='/yourID'><button ><h3>Build an identity</h3></button></Link>
             </div>
         </div>
     )
